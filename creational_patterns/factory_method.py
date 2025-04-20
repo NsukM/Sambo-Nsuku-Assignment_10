c2c36a8f68c4cb4592caab5ac2116628876ea84a
NsukM <105019575+NsukM@users.noreply.github.com>
2025-04-20 22:14:38 +0200

    Update factory_method.py

--- creational_patterns/factory_method.py
+++ creational_patterns/factory_method.py
@@ -1,1 +1,29 @@
+from abc import ABC, abstractmethod
+
+class Recommendation(ABC):
+    @abstractmethod
+    def generate(self):
+        pass
+
+class SleepRecommendation(Recommendation):
+    def generate(self):
+        return "You need 8 hours of sleep tonight."
+
+class ActivityRecommendation(Recommendation):
+    def generate(self):
+        return "Try walking 10,000 steps today."
+
+class RecommendationGenerator(ABC):
+    @abstractmethod
+    def create_recommendation(self):
+        pass
+
+class SleepRecGenerator(RecommendationGenerator):
+    def create_recommendation(self):
+        return SleepRecommendation()
+
+class ActivityRecGenerator(RecommendationGenerator):
+    def create_recommendation(self):
+        return ActivityRecommendation()
+
 
