d567397692adba74457b8ae7da4b5b47e20e8311
NsukM <105019575+NsukM@users.noreply.github.com>
2025-04-20 22:15:09 +0200

    Update abstract_factory.py

--- creational_patterns/abstract_factory.py
+++ creational_patterns/abstract_factory.py
@@ -1,1 +1,31 @@
+from abc import ABC, abstractmethod
+
+class Report(ABC):
+    @abstractmethod
+    def generate(self): pass
+
+class Summary(ABC):
+    @abstractmethod
+    def show(self): pass
+
+class DailyReport(Report):
+    def generate(self):
+        return "Generated daily report."
+
+class WeeklySummary(Summary):
+    def show(self):
+        return "Weekly summary shown."
+
+class ReportFactory(ABC):
+    @abstractmethod
+    def create_report(self): pass
+    @abstractmethod
+    def create_summary(self): pass
+
+class FitnessDataFactory(ReportFactory):
+    def create_report(self):
+        return DailyReport()
+
+    def create_summary(self):
+        return WeeklySummary()
 
