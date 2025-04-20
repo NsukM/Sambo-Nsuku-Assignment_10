4f1ff37266c8576bf34f54a48da976d2cd4d21b2
NsukM <105019575+NsukM@users.noreply.github.com>
2025-04-20 22:13:59 +0200

    Update simple_factory.py

--- creational_patterns/simple_factory.py
+++ creational_patterns/simple_factory.py
@@ -1,1 +1,11 @@
+class VitalStat:
+    def _init_(self, stat_type, value):
+        self.stat_type = stat_type
+        self.value = value
+
+class VitalStatFactory:
+    @staticmethod
+    def create_stat(stat_type, value):
+        return VitalStat(stat_type, value)
+
 
