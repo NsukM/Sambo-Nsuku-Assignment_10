6c28915423a60b43eaed29ec0560d510c012d9c0
NsukM <105019575+NsukM@users.noreply.github.com>
2025-04-20 22:02:32 +0200

    Update activity_session.py

--- src/activity_session.py
+++ src/activity_session.py
@@ -1,1 +1,18 @@
+class ActivitySession:
+    def _init_(self, session_id, date, duration, steps, calories):
+        self.session_id = session_id
+        self.date = date
+        self.duration = duration
+        self.steps = steps
+        self.calories = calories
+
+    def start_session(self):
+        return "Session started."
+
+    def end_session(self):
+        return "Session ended."
+
+    def sync(self):
+        return "Session synced."
+
 
