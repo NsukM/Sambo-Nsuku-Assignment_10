9a77d7c57fd6862297341eccb27b2cca42ff1ae6
NsukM <105019575+NsukM@users.noreply.github.com>
2025-04-20 22:16:07 +0200

    Update prototype.py

--- creational_patterns/prototype.py
+++ creational_patterns/prototype.py
@@ -1,1 +1,22 @@
+import copy
+
+class Recommendation:
+    def _init(self, rec_id, type, content, status="new"):
+        self.rec_id = rec_id
+        self.type = type_
+        self.content = content
+        self.status = status
+
+    def clone(self):
+        return copy.deepcopy(self)
+
+class RecommendationCache:
+    def _init_(self):
+        self.cache = {}
+
+    def add(self, key, recommendation):
+        self.cache[key] = recommendation
+
+    def get(self, key):
+        return self.cache[key].clone()
 
