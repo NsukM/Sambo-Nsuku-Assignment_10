7d2e5e235ba1862bb33c77fdcd68b186e48ac941
NsukM <105019575+NsukM@users.noreply.github.com>
2025-04-20 22:17:45 +0200

    Update test_singleton.py

--- tests/test_singleton.py
+++ tests/test_singleton.py
@@ -1,1 +1,11 @@
+from creational_patterns.singleton import DatabaseConnection
+
+def test_singleton_instance():
+    db1 = DatabaseConnection()
+    db2 = DatabaseConnection()
+    assert db1 is db2
+    assert db1.connect() == "Connected to DB"
+    assert db2.connected is True
+
+
 
