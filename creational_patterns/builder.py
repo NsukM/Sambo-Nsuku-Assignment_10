57e6c071fef3ef4f72d79bd76c000c0efa59aa51
NsukM <105019575+NsukM@users.noreply.github.com>
2025-04-20 22:15:43 +0200

    Update builder.py

--- creational_patterns/builder.py
+++ creational_patterns/builder.py
@@ -1,1 +1,33 @@
+class FitnessProfile:
+    def _init_(self):
+        self.age = None
+        self.weight = None
+        self.height = None
+        self.activity_level = None
+
+    def _str_(self):
+        return f"Profile: {self.age}y, {self.weight}kg, {self.height}cm, {self.activity_level}"
+
+class FitnessProfileBuilder:
+    def _init_(self):
+        self.profile = FitnessProfile()
+
+    def set_age(self, age):
+        self.profile.age = age
+        return self
+
+    def set_weight(self, weight):
+        self.profile.weight = weight
+        return self
+
+    def set_height(self, height):
+        self.profile.height = height
+        return self
+
+    def set_activity_level(self, level):
+        self.profile.activity_level = level
+        return self
+
+    def build(self):
+        return self.profile
 
