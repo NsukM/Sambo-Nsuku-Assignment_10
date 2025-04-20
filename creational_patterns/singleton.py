e079276cb875b13205e864e89f01c2c5bb7301a0
NsukM <105019575+NsukM@users.noreply.github.com>
2025-04-20 22:16:34 +0200

    Update singleton.py

--- creational_patterns/singleton.py
+++ creational_patterns/singleton.py
@@ -1,1 +1,18 @@
 
+class DatabaseConnection:
+    __instance = None
+
+    def _new_(cls):
+        if cls.__instance is None:
+            cls._instance = super(DatabaseConnection, cls).new_(cls)
+            cls.__instance.connected = False
+        return cls.__instance
+
+    def connect(self):
+        self.connected = True
+        return "Connected to DB"
+
+    def disconnect(self):
+        self.connected = False
+        return "Disconnected"
+
