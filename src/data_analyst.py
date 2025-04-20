308e6c8e7d98a0b2b46e3407cf33abb0488969f6
NsukM <105019575+NsukM@users.noreply.github.com>
2025-04-20 22:13:07 +0200

    Update data_analyst.py

--- src/data_analyst.py
+++ src/data_analyst.py
@@ -1,1 +1,13 @@
+class DataAnalyst:
+    def _init_(self, analyst_id, name, access_level):
+        self.analyst_id = analyst_id
+        self.name = name
+        self.access_level = access_level
+
+    def review_stat(self):
+        return "Stat reviewed."
+
+    def manage_reports(self):
+        return "Reports managed."
+
 
